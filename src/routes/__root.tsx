2bacd064aad302c571e2998ae540b368ddbe6afd
hammadtariq838 <24100267@lums.edu.pk>
2024-05-19 09:42:06 +0500

    fix: ui related issues

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,7 +1,6 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { Toaster } from '@/components/ui/sonner';
 import { lazy, Suspense } from 'react';
-import Navbar from '@/components/Navbar';
 const TanStackRouterDevtools =
   process.env.NODE_ENV === 'production'
     ? () => null
@@ -15,13 +14,7 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <Toaster richColors position="top-center" />
-      <main className="flex flex-col mx-auto min-h-screen bg-[F9F9FB]">
-        <Navbar />
-        <div className="h-10 bg-white mx-[53px]" />
-        <div className='flex bg-[#F2DCA6] grow pt-2 px-2 mx-[53px]'>
-          <Outlet />
-        </div >
-      </main >
+      <Outlet />
       <Suspense>
         <TanStackRouterDevtools />
       </Suspense>
--- src/routes/_protected.tsx
+++ src/routes/_protected.tsx
@@ -1,10 +1,19 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { useAppSelector } from '@/app/hooks';
 import { Navigate, Outlet } from '@tanstack/react-router';
+import Navbar from '@/components/Navbar';
 
 const PrivateRoute = () => {
   const { user } = useAppSelector((state) => state.auth);
-  return user ? <Outlet /> : <Navigate to="/sign-in" replace />;
+  return user ? (
+    <main className="flex flex-col mx-auto min-h-screen bg-[F9F9FB]">
+      <Navbar />
+      <div className="h-10 bg-white mx-[53px]" />
+      <div className='flex bg-[#F2DCA6] grow pt-2 px-2 mx-[53px]'>
+        <Outlet />
+      </div >
+    </main >
+  ) : <Navigate to="/sign-in" replace />;
 }
 
 export const Route = createFileRoute('/_protected')({
