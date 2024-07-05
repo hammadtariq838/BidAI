87ee60a29ac03ebc11552a7bbf36b2f9e2279ae6
hammadtariq838 <24100267@lums.edu.pk>
2024-07-05 20:06:19 +0500

    feat: domain updated

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const BASE_URL =
   import.meta.env.MODE === 'development'
     ? 'http://localhost:5000'
-    : 'https://api.bidai.hammadtariq.me';
+    : 'https://api.strux.ai';
 
 export const USERS_URL = `${BASE_URL}/api/users`;
 export const TENDERS_URL = `${BASE_URL}/api/tenders`;
